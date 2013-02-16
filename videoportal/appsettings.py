9ab92edbf80bcc5d78c14906733781dfe28a85f8
coding4coffee remote <remote@coding4coffee.org>
2013-02-16 20:49:32 +0100

    Grundlegende Anpassungen fuer TheRadio.CC

--- videoportal/appsettings.py
+++ videoportal/appsettings.py
@@ -11,7 +11,7 @@ TRANSLOAD_TEMPLATE_AUDIO_ID = '2e3fb41355d84acf8a08eff53c8fe74c'
 TRANSLOAD_TEMPLATE_VIDEO_AUDIO_ID = '52d9861b7683484d9eb8af5208c020e0'
 
 # The URL Transloadit should notify if it was done (please remember the trailing slash)
-TRANSLOAD_NOTIFY_URL = 'http://owntube.piratenfraktion-berlin.de/encodingdone/'
+TRANSLOAD_NOTIFY_URL = 'http://rec.theradio.cc/encodingdone/'
 
 TRANSLOAD_MP4_ENCODE = 'encode_iphone'
 TRANSLOAD_WEBM_ENCODE = 'encode_webm'
@@ -19,18 +19,18 @@ TRANSLOAD_MP3_ENCODE = 'encode_mp3'
 TRANSLOAD_OGG_ENCODE = 'encode_ogg'
 TRANSLOAD_THUMB_ENCODE = 'create_thumb'
 
-ENCODING_OUTPUT_DIR = '/mnt/iscsi0/media/encoded/'
+ENCODING_OUTPUT_DIR = '/data/srv/OwnTube/media/encoded/'
 # How can we reach this files (public access is needed)
-ENCODING_VIDEO_BASE_URL = 'http://owntube.piratenfraktion-berlin.de/media/encoded/'
+ENCODING_VIDEO_BASE_URL = 'http://rec.theradio.cc/media/encoded/'
 
-USE_BITTORRENT = True
+USE_BITTORRENT = False
 BITTORRENT_TRACKER_ANNOUNCE_URL = 'udp://tracker.publicbt.com:80'
 BITTORRENT_TRACKER_BACKUP = 'udp://tracker.openbittorrent.com:80,udp://tracker.ccc.de:80,udp://tracker.istole.it:80'
-BITTORRENT_FILES_DIR = '/opt/owntube/owntube/media/torrents/'
+BITTORRENT_FILES_DIR = '/data/srv/OwnTube/owntube/media/torrents/'
 # Where does transmission expects the original files? (This directory must be writeable for both transmission and owntube!)
 BITTORRENT_DOWNLOADS_DIR = '/opt/transmission-folder'
 # What is the URL of the BITTORRENT_FILES_DIR?
-BITTORRENT_FILES_BASE_URL = 'http://owntube.piratenfraktion-berlin.de/media/torrents/'
+BITTORRENT_FILES_BASE_URL = 'http://rec.theradio.cc/media/torrents/'
 
 # Host and port Transmission is listining on (probably localhost
 TRANSMISSION_HOST = '127.0.0.1'
@@ -38,5 +38,5 @@ TRANSMISSION_PORT = 9091
 
 # Base-Dir vor Hotfolders
 
-HOTFOLDER_BASE_DIR = '/mnt/iscsi0/videostore/robotron/'
-HOTFOLDER_MOVE_TO_DIR = '/opt/owntube/owntube/media/raw/'
+HOTFOLDER_BASE_DIR = '/data/srv/OwnTube/owntube/videostore/robotron/'
+HOTFOLDER_MOVE_TO_DIR = '/data/srv/OwnTube/owntube/media/raw/'
