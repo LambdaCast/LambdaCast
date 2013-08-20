7df81a8c2e79fa4304de35134fdc13faaeaddd79
Dennis <account@d2ns.org>
2013-08-20 04:14:51 +0200

    code cleaning, deleting old parts of static pages

--- static_pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django import forms
-from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse
-
-import lambdaproject.settings as settings
-
