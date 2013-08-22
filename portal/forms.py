e5e182d5281e3b0c75dc05a0e9d0ae514e95c68c
Dennis <account@d2ns.org>
2013-08-23 01:04:23 +0200

    function in forms.py out of VideoForms object

--- portal/forms.py
+++ portal/forms.py
@@ -8,6 +8,20 @@ import lambdaproject.settings as settings
 import os
 import os.path
 
+def getThumbnails(thumbssettings):
+    ''' Lists all thumbnails from the /media/thumbnails-directory and sends them to the videoThumbURL and audioThumbURL '''
+    thumbsdir = thumbssettings
+    if not os.path.exists(thumbsdir):
+        os.makedirs(thumbsdir)
+    thumbsdir = thumbsdir + '/'
+    thumb_list = []
+    for thumbfile in os.listdir(thumbsdir):
+        if os.path.isfile(os.path.join(thumbsdir, thumbfile)):
+            thumb_list.append((("http://" + thumbfile),("http://" + thumbfile)))
+    return thumb_list
+
+THUMBNAILS_LIST = getThumbnails(settings.THUMBNAILS_DIR)
+
 class VideoForm(ModelForm):
     ''' Used for the uploading form '''
 
@@ -16,7 +30,6 @@ class VideoForm(ModelForm):
         exclude = ["slug","mp4URL","mp4Size","flashURL","flashSize","webmURL","webmSize","mp3URL","mp3Size","oggURL","oggSize","ogvURL","ogvSize","duration","published","encodingDone","assemblyid","torrentURL","user","autoPublish", "torrentDone"]
 
     def __init__(self, *args, **kwargs):
-	THUMBNAILS_LIST = self.getThumbnails(settings.THUMBNAILS_DIR)
         super(VideoForm, self).__init__(*args, **kwargs)
         for fieldName in self.fields:
             if fieldName == 'audioThumbURL':
@@ -26,18 +39,6 @@ class VideoForm(ModelForm):
                 if field.required:
                     field.widget.attrs['class'] = 'required'
 
-    def getThumbnails(self,thumbssettings):
-        ''' Lists all thumbnails from the /media/thumbnails-directory and sends them to the videoThumbURL and audioThumbURL '''
-        thumbsdir = thumbssettings
-        if not os.path.exists(thumbsdir):
-            os.makedirs(thumbsdir)
-        thumbsdir = thumbsdir + '/'
-        thumb_list = []
-        for thumbfile in os.listdir(thumbsdir):
-            if os.path.isfile(os.path.join(thumbsdir, thumbfile)):
-                thumb_list.append((("http://" + thumbfile),("http://" + thumbfile)))
-            print thumb_list
-        return thumb_list
 
 class CommentForm(ModelForm):
     ''' Used for the comments '''
