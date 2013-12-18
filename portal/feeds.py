24915b44d8bc9cd7fb52af5c216adfe333cfb63a
TheRadialActive <account@d2ns.org>
2013-12-19 00:12:15 +0100

    LatestVideos with iTunes support

--- portal/feeds.py
+++ portal/feeds.py
@@ -1,8 +1,11 @@
 from django.contrib.syndication.views import Feed, FeedDoesNotExist
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
+from django.utils.feedgenerator import *
 
 from portal.models import Video, Channel, Collection
+
+from django.utils.feedgenerator import Rss201rev2Feed
 
 import lambdaproject.settings as settings
 
@@ -10,22 +13,58 @@ import markdown
 
 import os
 
+class iTunesFeed(Rss201rev2Feed):
+    def rss_attributes(self):
+        return {u"version": self._version, u"xmlns:content": "http://purl.org/rss/1.0/modules/content", u"xmlns:sy": "http://purl.org/rss/1.0/modules/syndication/", u"xmlns:atom": u"http://www.w3.org/2005/Atom", u'xmlns:itunes': u'http://www.itunes.com/dtds/podcast-1.0.dtd'}
+
+    def add_root_elements(self, handler):
+        super(iTunesFeed, self).add_root_elements(handler)
+        handler.addQuickElement(u'itunes:subtitle', self.feed['subtitle'])
+        handler.addQuickElement(u'itunes:author', self.feed['author_name'])
+        handler.addQuickElement(u'itunes:summary', self.feed['description'])
+        handler.addQuickElement(u'itunes:explicit', self.feed['iTunes_explicit'])
+        handler.startElement(u"itunes:owner", {})
+        handler.addQuickElement(u'itunes:name', self.feed['iTunes_name'])
+        handler.addQuickElement(u'itunes:email', self.feed['iTunes_email'])
+        handler.endElement(u"itunes:owner")
+        handler.addQuickElement(u'itunes:image', self.feed['iTunes_image_url'])
+        
+
+    def add_item_elements(self,  handler, item):
+        super(iTunesFeed, self).add_item_elements(handler, item)
+        handler.addQuickElement(u'iTunes:summary',item['summary'])
+        handler.addQuickElement(u'iTunes:duration',item['duration'])
+        handler.addQuickElement(u'iTunes:explicit',item['explicit'])
+
 class LatestVideos(Feed):
+    feed_type = iTunesFeed
     title = _("LambdaCast Latest Videos")
     link = "/"
     description = _(u"The newest media from LambdaCast")
-
+    subtitle = description
+    author_name = str(settings.AUTHOR_NAME)
+  
     def get_object(self, request, fileformat):
         self.fileformat = fileformat
 
-    def item_enclosure_mime_type(self):
-        if self.fileformat == 'mp4' or self.fileformat == 'webm':
-            return 'video/%s' % self.fileformat
-        else:
-            return 'audio/%s' % self.fileformat
-    
     def items(self):
         return Video.objects.filter(published=True).exclude(mp3URL='', oggURL='', webmURL='', mp4URL='').order_by('-created')
+
+    def feed_extra_kwargs(self, obj):
+        extra = {}
+        extra['iTunes_name'] = str(settings.AUTHOR_NAME)
+        extra['iTunes_email'] = str(settings.CONTACT_EMAIL)
+        extra['iTunes_image_url'] = str(settings.LOGO_URL)
+        extra['iTunes_explicit'] = 'no'
+        return extra
+
+    def item_extra_kwargs(self, item):
+        extra = {}
+        extra['duration'] = str(item.duration)
+        extra['summary'] = str(item.description)
+        extra['explicit'] = 'no'
+        return extra
+
 
     def item_title(self, item):
         return item.title
@@ -44,7 +83,20 @@ class LatestVideos(Feed):
             return item.webmURL
         else:
             return Exception
-            
+    
+    def item_link(self, item):
+        if self.fileformat == 'mp3':
+            return item.mp3URL
+        elif self.fileformat == 'mp4':
+            return item.mp4URL
+        elif self.fileformat == 'ogg':
+            return item.oggURL
+        elif self.fileformat == 'webm':
+            return item.webmURL
+        else:
+            return Exception            
+
+
     def item_enclosure_length(self, item):
         if self.fileformat == 'mp3':
             return item.mp3Size
@@ -59,6 +111,16 @@ class LatestVideos(Feed):
 
     def item_pubdate(self, item):
         return item.created
+
+    def item_enclosure_length(self, item):
+        return item.duration
+
+    def item_enclosure_mime_type(self):
+        if self.fileformat == 'mp4' or self.fileformat == 'webm':
+            return 'video/%s' % self.fileformat
+        else:
+            return 'audio/%s' % self.fileformat
+
 
 class TorrentFeed(Feed):
     title = _(u"LambdaCast TorrentFeed")
