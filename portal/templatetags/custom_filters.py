30a9a4699bab1d08a5765005b914c009404119a3
TheRadialActive <account@d2ns.org>
2014-01-11 19:33:40 +0100

    get remote file size mp3

--- portal/templatetags/custom_filters.py
+++ portal/templatetags/custom_filters.py
@@ -1,5 +1,6 @@
 from django import template
 import datetime
+import urllib
 
 from django.utils.text import normalize_newlines
 
@@ -16,3 +17,10 @@ def remove_newlines(text):
     ''' Removes all newline characters from a block of text.'''
     normalized_text = normalize_newlines(text)
     return normalized_text.replace('\n', ' ')
+
+@register.filter(name="get_remote_size")
+def get_remote_size(url):
+    site = urllib.urlopen(url)
+    meta = site.info()
+    size_in_mb = float(meta.getheaders("Content-Length")[0]) / 1024 / 1024 
+    return round(size_in_mb, 2)
