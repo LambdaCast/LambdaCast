e6544290d859e8d00905f33cf053f5ab9fc2528a
TheRadialActive <account@d2ns.org>
2014-01-16 18:40:26 +0100

    changing test db name, utf8 encoding for dbs

--- lambdaproject/settings/base.py
+++ lambdaproject/settings/base.py
@@ -43,11 +43,14 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lambda.sql',            # Or path to database file if using sqlite3.
+        'NAME': 'test.sql',            # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'STORAGE_ENGINE': 'INNODB',
+        'CHARACTER_SET_CONNECTION': 'utf8',
+        'COLLATION_CONNECTION': 'utf8_unicode_ci',
     }
 }
 
