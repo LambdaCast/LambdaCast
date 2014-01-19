d14081a2611a52098210b25c8ef2d5dd39d4aabc
Benjamin Neff <benjamin@coding4coffee.ch>
2014-01-19 17:57:03 +0100

    add utf-8 to examples config

--- lambdaproject/settings/base.py
+++ lambdaproject/settings/base.py
@@ -43,7 +43,7 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.sql',            # Or path to database file if using sqlite3.
+        'NAME': 'test.sql',              # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
--- lambdaproject/settings/local.example.py
+++ lambdaproject/settings/local.example.py
@@ -20,8 +20,8 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 # If you use an virtualenv (you schould) enter it here
-VIRTUALENV = ABSOLUTE_PATH + '/.venv/lib/python2-6/sites-packages'
-# VIRTUALENV = ABSOLUTE_PATH + '/.venv/lib/python2.7/site-packages'
+# VIRTUALENV = ABSOLUTE_PATH + '/.venv/lib/python2-6/sites-packages'
+VIRTUALENV = ABSOLUTE_PATH + '/.venv/lib/python2.7/site-packages'
 
 # The guys who will get an email if something is wrong
 ADMINS = (
@@ -32,11 +32,14 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lambda.sql',            # Or path to database file if using sqlite3.
+        'NAME': 'test.sql',              # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'STORAGE_ENGINE': 'INNODB',
+        'CHARACTER_SET_CONNECTION': 'utf8',
+        'COLLATION_CONNECTION': 'utf8_unicode_ci',
     }
 }
 
