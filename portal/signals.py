a541a88d14b0c9be94f8069a3052480d3dfa4307
Benjamin Neff <benjamin@coding4coffee.ch>
2014-01-25 23:36:41 +0100

    LAMBDA-69: refactoring signals.py

--- portal/signals.py
+++ portal/signals.py
@@ -2,20 +2,17 @@ from django.db.models.signals import pre_save
 import urllib2
 
 def get_remote_filesize(sender, instance, **kwargs):
-    format_list = [instance.mp3URL, instance.mp4URL, instance.webmURL, instance.oggURL]
-    size_list = []
-    for url in format_list: 
-        try:
-            request = urllib2.Request(url)
-            request.get_method = lambda: 'HEAD'
+    instance.mp3Size = _get_remote_filesize_for_url(instance.mp3URL)
+    instance.oggSize = _get_remote_filesize_for_url(instance.oggURL)
+    instance.mp4Size = _get_remote_filesize_for_url(instance.mp4URL)
+    instance.webmSize = _get_remote_filesize_for_url(instance.webmURL)
 
-            response = urllib2.urlopen(request)
-            size_in_bytes = response.info().getheader('content-length')
-            size_list.append(size_in_bytes)
-        except:
-            size_list.append(0)
-    
-    instance.mp3Size = size_list[0]
-    instance.mp4Size = size_list[1]
-    instance.webmSize = size_list[2]
-    instance.oggSize = size_list[3]
+def _get_remote_filesize_for_url(url):
+    try:
+        request = urllib2.Request(url)
+        request.get_method = lambda: 'HEAD'
+
+        response = urllib2.urlopen(request)
+        return response.info().getheader('content-length')
+    except:
+        return 0
