54952bb49ddb21f43b37cba7dc0d89ffe61ab9eb
TheRadialActive <account@d2ns.org>
2014-01-26 05:09:40 +0100

    LAMBDA-27: portal/.../scanhotfolders renamed

--- portal/management/commands/scanhotfolders.py
+++ portal/management/commands/scanhotfolders.py
@@ -24,16 +24,16 @@ class Command(BaseCommand):
                 if mtime < five_minutes_ago:
                     if file.endswith(".mov") or file.endswith(".mp4") or file.endswith(".avi") or file.endswith(".ogv") or file.endswith(".m4v") or file.endswith(".mp3") or file.endswith(".ogg"):
                         self.stdout.write(_(u'Using file %s\n') % file) 
-                        video = Video(title=folder.defaultName,date=datetime.date.today(),description=folder.description,kind=folder.kind,channel=folder.channel,autoPublish=folder.autoPublish)
-                        video.save()
+                        mediaitem = MediaItem(title=folder.defaultName,date=datetime.date.today(),description=folder.description,kind=folder.kind,channel=folder.channel,autoPublish=folder.autoPublish)
+                        mediaitem.save()
                         shutil.copy(settings.HOTFOLDER_BASE_DIR + folder.folderName + '/' + file, settings.HOTFOLDER_MOVE_TO_DIR)
-                        video.originalFile = settings.HOTFOLDER_MOVE_TO_DIR + file
-                        video.save()
+                        mediaitem.originalFile = settings.HOTFOLDER_MOVE_TO_DIR + file
+                        mediaitem.save()
                         os.remove(settings.HOTFOLDER_BASE_DIR + folder.folderName + '/' + file)
-                        djangotasks.register_task(video.encode_media, _(u"Encode the files using ffmpeg"))
-                        encoding_task = djangotasks.task_for_object(video.encode_media)
+                        djangotasks.register_task(mediaitem.encode_media, _(u"Encode the files using ffmpeg"))
+                        encoding_task = djangotasks.task_for_object(mediaitem.encode_media)
                         djangotasks.run_task(encoding_task)
                         if settings.USE_BITTORRENT:
-                            djangotasks.register_task(video.create_bittorrent, _(u"Create Bittorrent file for media and serve via Bittorrent"))
-                            torrent_task = djangotasks.task_for_object(video.create_bittorrent)
+                            djangotasks.register_task(mediaitem.create_bittorrent, _(u"Create Bittorrent file for media and serve via Bittorrent"))
+                            torrent_task = djangotasks.task_for_object(mediaitem.create_bittorrent)
                             djangotasks.run_task(torrent_task)
