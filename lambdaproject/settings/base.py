b9b8384aeb765f8088b12a035bac492763a22ef0
Benjamin Neff <benjamin@coding4coffee.ch>
2014-06-04 22:06:12 +0200

    LAMBDA-156: create SubmittalMiddleware

--- lambdaproject/settings/base.py
+++ lambdaproject/settings/base.py
@@ -143,6 +143,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'portal.middleware.SubmittalMiddleware',
 )
 
 ROOT_URLCONF = 'lambdaproject.urls'
