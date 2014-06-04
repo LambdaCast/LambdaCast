cc99ff6c145d301560c3dd4b664f15e83f2c39b7
Benjamin Neff <benjamin@coding4coffee.ch>
2014-06-04 23:17:19 +0200

    FIX: RuntimeWarning: DateTimeField received a naive datetime while time zone support is active.

--- livestream/views.py
+++ livestream/views.py
@@ -3,14 +3,14 @@ from django.template.response import TemplateResponse
 
 from livestream.models import Stream
 
-import datetime
+from django.utils import timezone
 
 def current(request):
     ''' This view gets all streams that are scheduled to be live right now
     if there are no streams it redirects to the liste view. The upcoming_streams_list
     is used to show upcoming events in a side bar (see the template)'''
-    stream_list = Stream.objects.filter(published=True,startDate__lt=datetime.datetime.now, endDate__gt=datetime.datetime.now).order_by('-startDate')
-    upcoming_streams_list = Stream.objects.filter(published=True,endDate__gt=datetime.datetime.now).order_by('-startDate')[:5]
+    stream_list = Stream.objects.filter(published=True,startDate__lt=timezone.now, endDate__gt=timezone.now).order_by('-startDate')
+    upcoming_streams_list = Stream.objects.filter(published=True,endDate__gt=timezone.now).order_by('-startDate')[:5]
     if not stream_list:
         return redirect(list)
     else:
@@ -19,7 +19,7 @@ def current(request):
 def list_streams(request):
     ''' This view shows gets all upcoming streaming events
     and forwards them to our template '''
-    stream_list = Stream.objects.filter(published=True,endDate__gt=datetime.datetime.now).order_by('-startDate')
+    stream_list = Stream.objects.filter(published=True,endDate__gt=timezone.now).order_by('-startDate')
     return TemplateResponse(request, 'livestream/list.html', {'stream_list': stream_list})
 
 def detail(request, slug):
@@ -27,5 +27,5 @@ def detail(request, slug):
     show the user more information on one event but not
     for showing the player'''
     stream = get_object_or_404(Stream, slug=slug)
-    upcoming_streams_list = Stream.objects.filter(published=True,endDate__gt=datetime.datetime.now).order_by('-startDate')[:5]
+    upcoming_streams_list = Stream.objects.filter(published=True,endDate__gt=timezone.now).order_by('-startDate')[:5]
     return TemplateResponse(request, 'livestream/detail.html', {'stream': stream, 'upcoming_streams_list': upcoming_streams_list})
