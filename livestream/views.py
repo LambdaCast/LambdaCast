e6618f2017ca1c33a1a733b56b14fe9d6e1cbc2a
Benjamin Neff <benjamin@coding4coffee.ch>
2014-06-04 23:05:39 +0200

    LAMBDA-156: refactoring views with TemplateResponse

--- livestream/views.py
+++ livestream/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django import forms
-from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
-
-import lambdaproject.settings as settings
+from django.shortcuts import get_object_or_404, redirect
+from django.template.response import TemplateResponse
 
 from livestream.models import Stream
 
@@ -19,15 +14,13 @@ def current(request):
     if not stream_list:
         return redirect(list)
     else:
-        return render_to_response('livestream/current.html', {'stream_list': stream_list, 'upcoming_streams_list': upcoming_streams_list, 'settings': settings},
-                            context_instance=RequestContext(request))
+        return TemplateResponse(request, 'livestream/current.html', {'stream_list': stream_list, 'upcoming_streams_list': upcoming_streams_list})
 
-def list(request):
+def list_streams(request):
     ''' This view shows gets all upcoming streaming events
     and forwards them to our template '''
     stream_list = Stream.objects.filter(published=True,endDate__gt=datetime.datetime.now).order_by('-startDate')
-    return render_to_response('livestream/list.html', {'stream_list': stream_list, 'settings': settings},
-                            context_instance=RequestContext(request))
+    return TemplateResponse(request, 'livestream/list.html', {'stream_list': stream_list})
 
 def detail(request, slug):
     ''' This view shows the detail of a stream, it is used to
@@ -35,5 +28,4 @@ def detail(request, slug):
     for showing the player'''
     stream = get_object_or_404(Stream, slug=slug)
     upcoming_streams_list = Stream.objects.filter(published=True,endDate__gt=datetime.datetime.now).order_by('-startDate')[:5]
-    return render_to_response('livestream/detail.html', {'stream': stream, 'upcoming_streams_list': upcoming_streams_list, 'settings': settings},
-                            context_instance=RequestContext(request))
+    return TemplateResponse(request, 'livestream/detail.html', {'stream': stream, 'upcoming_streams_list': upcoming_streams_list})
