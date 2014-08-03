b0542f0abba2b574a605ff70ef89e631e504bbd2
Dennis <account@d2ns.org>
2014-08-03 18:34:08 +0200

    LAMBDA-56: rough bs3 update of admin and index

--- lambdaproject/settings/base.py
+++ lambdaproject/settings/base.py
@@ -167,6 +167,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_admin_bootstrapped.bootstrap3',
     'django_admin_bootstrapped',
     'django.contrib.admin',
     #'django.contrib.admindocs',
