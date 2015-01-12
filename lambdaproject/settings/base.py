216031e08cedf45c694f67f6270ec0f31004ebb1
Benjamin Neff <benjamin@coding4coffee.ch>
2015-01-12 02:31:39 +0100

    LAMBDA-173: fix TEMPLATE_DIRS

--- lambdaproject/settings/base.py
+++ lambdaproject/settings/base.py
@@ -159,7 +159,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ABSOLUTE_PATH, 'templates'
+    ABSOLUTE_PATH + '/templates',
 )
 
 INSTALLED_APPS = (
