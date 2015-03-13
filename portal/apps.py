cd5dc7f04415787fdac085849d0dc4bfea087ac2
Dennis <account@d2ns.org>
2015-03-13 17:03:28 +0100

    LAMBDA-196: 'upload'-dir for tmp, seperate ifs

--- portal/apps.py
+++ portal/apps.py
@@ -12,7 +12,7 @@ class Portal(AppConfig):
     def ready(self):
         if not os.path.isdir(settings.MEDIA_ROOT):
             os.makedirs(settings.MEDIA_ROOT)
-        elif not os.path.isdir(settings.FILE_UPLOAD_TEMP_DIR):
+        if not os.path.isdir(settings.FILE_UPLOAD_TEMP_DIR):
             os.makedirs(settings.FILE_UPLOAD_TEMP_DIR)
-        elif not os.path.isdir(settings.ENCODING_OUTPUT_DIR):
+        if not os.path.isdir(settings.ENCODING_OUTPUT_DIR):
             os.makedirs(settings.ENCODING_OUTPUT_DIR)
